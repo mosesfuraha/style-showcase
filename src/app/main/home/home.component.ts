3a4013076100446089c2dcd298a9676761a14706
mosesfuraha <moses.furaha@amalitechtraining.org>
2024-09-11 08:50:33 +0200

    create lazy loading

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+// src/app/main/home/home.component.ts
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+interface SliderItem {
+  id: number;
+  title: string;
+  description: string;
+  imageUrl: string;
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], // Import CommonModule for Angular directives
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  sliderItems: SliderItem[] = [];
+  errorMessage: string = '';
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchSliderData();
+  }
+
+  fetchSliderData(): void {
+    this.http.get<{ slider: SliderItem[] }>('assets/slider.json').subscribe({
+      next: (data) => {
+        this.sliderItems = data.slider;
+      },
+      error: (error) => {
+        this.errorMessage = 'Error fetching slider data.';
+        console.error('Error fetching slider data:', error);
+      },
+    });
+  }
 }
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
+import { ProductComponent } from './product/product.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, MainRoutingModule],
+  declarations: [
+    ProductComponent
+  ],
+  imports: [CommonModule, MainRoutingModule, HttpClientModule],
 })
 export class MainModule {}
